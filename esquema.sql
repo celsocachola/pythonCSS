09b5918878f4b7c32b3fc07787af9095db29eeb7
celsocachola <celsocachola@gmail.com>
2021-04-29 16:15:52 +0000

    Commite

--- esquema.sql
+++ esquema.sql
@@ -1,5 +1,4 @@
-drop table if exists entradas;
-create table entradas (
+create table if not exists entradas (
   id integer primary key autoincrement,
   titulo string not null,
   texto string not null
--- flaskr.py
+++ flaskr.py
@@ -1,4 +1,5 @@
 import sqlite3
+from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
 
@@ -19,6 +20,8 @@ def criar_bd():
 def conectar_bd():
     return sqlite3.connect(DATABASE)
 
+criar_bd()
+
 @app.before_request
 def pre_requisicao():
     g.bd = conectar_bd()
@@ -27,6 +30,38 @@ def pre_requisicao():
 def encerrar_requisicao(exception):
     g.bd.close()
 
+
 @app.route('/')
-def index():
-    return "<h1>Roberto ama seu norte<h1>"
+def exibir_entradas():
+    sql = "SELECT titulo, texto FROM entradas ORDER BY id DESC"
+    cursor = g.bd.execute(sql)
+    entradas = [dict(titulo=titulo, texto=texto) for titulo, texto in cursor.fetchall()]
+    return render_template('exibir_entradas.html', entradas=entradas)
+
+@app.route('/inserir', methods=['POST'])
+def inserir_entradas():
+    if not session.get('logado'):
+        abort(401)
+    sql = "INSERT INTO entradas(titulo, texto) VALUES (?, ?)"
+    g.bd.execute(sql, [request.form['titulo'], request.form['texto']])
+    g.bd.commit()
+    flash("Nova entrada inserida com sucesso")
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form['username'] != USERNAME or request.form['password'] != PASSWORD:
+            erro = "Usuário ou senha inválidos"
+        else:
+            session['logado'] = True
+            flash('Bem vindo!')
+            return redirect(url_for('exibir_entradas'))
+    return render_template('login.html', erro=erro)
+
+@app.route('/logout')
+def logout():
+    session['logado'] = False
+    flash('Saiu com sucesso!')
+    return redirect(url_for('exibir_entradas'))
